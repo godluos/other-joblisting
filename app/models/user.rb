99b5b4c2cf14de6e3c5ff8c5cd102a36e971fe05
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-04-23 02:53:07 +0800

    implement group relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :resumes
+  has_many :job_relationships
+  has_many :participated_jobs, :through => :job_relationships, :source => :job
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
